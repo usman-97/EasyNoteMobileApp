0f746a5651eae3731b3f490870c43e3280075c01
Usman <usmanshabir65@hotmail.com>
2021-12-25 04:08:34 +0000

    start text added

--- lib/views/loading_screen.dart
+++ lib/views/loading_screen.dart
@@ -11,12 +11,43 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFF388E3c),
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           mainAxisAlignment: MainAxisAlignment.center,
           children: const <Widget>[
-            Text('EasyNote'),
+            Center(
+              child: Text(
+                'EasyNote',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 60.0,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 50.0,
+              child: Center(
+                child: Divider(
+                  indent: 50.0,
+                  endIndent: 50.0,
+                  color: Color(0xFFC8E6C9),
+                  thickness: 5.0,
+                ),
+              ),
+            ),
+            Center(
+              child: Text(
+                'Capture your ideas',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+            ),
           ],
         ),
       ),
