28b532360eeae682e669f2948af48bc9cb8ba783
Usman <usmanshabir65@hotmail.com>
2022-01-01 03:39:43 +0000

    loadingScreen constant Textstyle added

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const TextStyle kMainHeadingStyle = TextStyle(
+  color: Colors.white,
+  fontFamily: 'Pushster',
+  fontSize: 70.0,
+);
+
+const TextStyle kMainHeadingSubtitle = TextStyle(
+  color: Colors.white,
+  fontFamily: 'Indie Flower',
+  fontSize: 25.0,
+  fontStyle: FontStyle.italic,
+);
