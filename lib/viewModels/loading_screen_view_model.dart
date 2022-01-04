a0d75881fb61ba3d7e79cbd1121f79461a9ee85e
Usman <usmanshabir65@hotmail.com>
2022-01-04 04:35:09 +0000

    initial commit

--- lib/viewModels/loading_screen_view_model.dart
+++ lib/viewModels/loading_screen_view_model.dart
@@ -3,7 +3,7 @@ import 'package:note_taking_app/views/login_screen.dart';
 
 class LoadingScreenViewModel {
   void navigateToLoginScreen(BuildContext context) {
-    Future.delayed(const Duration(seconds: 2), () {
+    Future.delayed(const Duration(seconds: 2, milliseconds: 500), () {
       Navigator.pushNamed(context, LoginScreen.id);
     });
   }
