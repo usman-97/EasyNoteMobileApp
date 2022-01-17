7df658d1fa771f400c3628397d2dea0adc186704
Usman <usmanshabir65@hotmail.com>
2022-01-17 03:49:46 +0000

    firebase mock initilisation added in the test

--- test/widget_test/register_screen_test.dart
+++ test/widget_test/register_screen_test.dart
@@ -1,23 +1,57 @@
+import 'dart:ui';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:note_taking_app/components/round_button.dart';
 import 'package:note_taking_app/views/register_screen.dart';
+import 'package:note_taking_app/services/initialise_firebase_mock.dart';
 
 void main() {
-  testWidgets('Register Screen', (WidgetTester tester) async {
-    // Build RegisterScreen widget
-    await tester.pumpWidget(const MaterialApp(
-      home: RegisterScreen(),
-    ));
+  // TestWidgetsFlutterBinding.ensureInitialized();
+  //
+  // setUp(() async {
+  //   WidgetsFlutterBinding.ensureInitialized();
+  //   await Firebase.initializeApp();
+  // });
 
-    final heading = find.text('EasyNote');
-    final subtitle = find.text('Capture your ideas');
-    final registerButton = find.byType(RoundButton);
-    final textField = find.byType(TextField);
+  // MethodChannelFirebase.channel.setMockMethodCallHandler((call) async {
+  //   if (call.method == 'Firebase#initializeCore'){
+  //     return [
+  //       'name': defaultFirebaseAppName,
+  //       'options': {
+  //         'apiKey': '123',
+  //         'appId': '123',
+  //         'messagingSenderId': '123',
+  //         'projectId': '123',
+  //       },
+  //       'pluginConstants': {},
+  //     ];
+  //   }
+  // });
+  setupFirebaseAuthMocks();
 
-    expect(heading, findsOneWidget); // Find main heading
-    expect(subtitle, findsOneWidget); // Find subtitle
-    expect(registerButton, findsOneWidget); // Find register button
-    expect(textField, findsNWidgets(3)); // Find all textfields
+  group('Register Screen', () {
+    setUpAll(() async {
+      // WidgetsFlutterBinding.ensureInitialized();
+      await Firebase.initializeApp();
+    });
+
+    testWidgets('Register Screen', (WidgetTester tester) async {
+      // Build RegisterScreen widget
+      await tester.pumpWidget(const MaterialApp(
+        home: RegisterScreen(),
+      ));
+
+      final heading = find.text('EasyNote');
+      final subtitle = find.text('Capture your ideas');
+      final registerButton = find.byType(RoundButton);
+      final textField = find.byType(TextField);
+
+      expect(heading, findsOneWidget); // Find main heading
+      expect(subtitle, findsOneWidget); // Find subtitle
+      expect(registerButton, findsOneWidget); // Find register button
+      expect(textField, findsNWidgets(3)); // Find all textfields
+    });
   });
 }
