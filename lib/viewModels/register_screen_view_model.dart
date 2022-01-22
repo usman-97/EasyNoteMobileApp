b28e568c3d185cc7c9d1d2e05b3a509c1641d5ee
Usman <usmanshabir65@hotmail.com>
2022-01-22 02:39:27 +0000

    password validation added

--- lib/viewModels/register_screen_view_model.dart
+++ lib/viewModels/register_screen_view_model.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:note_taking_app/models/user_authentication.dart';
 import 'package:note_taking_app/views/login_screen.dart';
 
@@ -15,21 +16,72 @@ class RegisterScreenViewModel {
     Navigator.pushNamed(context, LoginScreen.id);
   }
 
-  bool _isEmailValid({required String email}) {
+  Future<void> registerUser(
+      String email, String password, String confirmPassword) async {
     if (email.isNotEmpty) {
-      return true;
+      if (password.isNotEmpty && _isPasswordStrong(password)) {
+        if (_isConfirmPasswordMatch(password, confirmPassword)) {
+          bool isUserRegistered =
+              await _userControl.registerUser(email: email, password: password);
+          if (!isUserRegistered) {
+            _error = 'Invalid Email/Password';
+          }
+        } else {
+          _error = 'Password does not match.';
+        }
+      } else {
+        _error =
+            'Your password should contain 8 or more characters including at least an uppercase letter, a number and a special character (!£€^@)';
+      }
+    } else {
+      _error = 'Invalid email';
     }
-    return false;
   }
 
-  Future<void> registerUser(
-      {required String email, required String password}) async {
-    if (_isEmailValid(email: email)) {
-      bool isUserRegistered =
-          await _userControl.registerUser(email: email, password: password);
-      if (!isUserRegistered) {
-        _error = 'Invalid Email/Password';
-      }
+  bool _isConfirmPasswordMatch(String password, String confirmPassword) {
+    if (password == confirmPassword) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  bool _isPasswordStrong(String password) {
+    if (_doesPasswordContainerUppercase(password) &&
+        _doesPasswordContainNumber(password) &&
+        _doesPasswordContainSymbol(password) &&
+        password.length >= 8) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  bool _doesPasswordContainerUppercase(String password) {
+    RegExp uppercaseMatch =
+        RegExp(r'[A-Z]', caseSensitive: false, multiLine: false);
+    if (uppercaseMatch.firstMatch(password) != null) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  bool _doesPasswordContainNumber(String password) {
+    RegExp findNumber = RegExp(r'[0-9]');
+    if (findNumber.firstMatch(password) != null) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  bool _doesPasswordContainSymbol(String password) {
+    RegExp findSymbol = RegExp(r'[!£€^&@]');
+    if (findSymbol.firstMatch(password) != null) {
+      return true;
+    } else {
+      return false;
     }
   }
 
