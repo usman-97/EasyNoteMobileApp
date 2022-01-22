de5fa77e6fa0cf8de7f19ece6d4fe970dbf8c06b
Usman <usmanshabir65@hotmail.com>
2022-01-22 01:38:18 +0000

    user_authenticated edited

--- lib/models/user_authentication.dart
+++ lib/models/user_authentication.dart
@@ -5,10 +5,12 @@ class UserAuthentication {
   final FirebaseAuth _firebaseAuthentication =
       FirebaseAuthentication.firebaseAuthInstance();
 
+  // Register new user using FirebaseAuth
   Future<bool> registerUser(
       {required String email, required String password}) async {
     bool isUserRegistered = false;
     try {
+      // Register new user
       final newUser =
           await _firebaseAuthentication.createUserWithEmailAndPassword(
         email: email,
