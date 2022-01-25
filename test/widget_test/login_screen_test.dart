a8315a986760bd52918cce4ea15d614e2c214ca6
Usman <usmanshabir65@hotmail.com>
2022-01-25 03:35:14 +0000

    login widget test edited

--- test/widget_test/login_screen_test.dart
+++ test/widget_test/login_screen_test.dart
@@ -1,23 +1,33 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:note_taking_app/services/initialise_firebase_mock.dart';
 import 'package:note_taking_app/views/login_screen.dart';
 import 'package:note_taking_app/components/round_button.dart';
 
 void main() {
-  testWidgets('Login Screen', (WidgetTester tester) async {
-    // Build LoginScreen Widget
-    await tester.pumpWidget(const MaterialApp(
-      home: LoginScreen(),
-    ));
+  setupFirebaseAuthMocks();
 
-    final heading = find.text('EasyNote');
-    final subtitle = find.text('Capture your ideas');
-    final loginButton = find.byType(RoundButton);
-    final textField = find.byType(TextField);
+  group('Login Screen', () {
+    setUpAll(() async {
+      await Firebase.initializeApp();
+    });
 
-    expect(loginButton, findsOneWidget); // Find login button
-    expect(heading, findsOneWidget); // Find main heading
-    expect(subtitle, findsOneWidget); // find subtitle
-    expect(textField, findsNWidgets(2)); // find 2 textfields
+    testWidgets('Login Screen', (WidgetTester tester) async {
+      // Build LoginScreen Widget
+      await tester.pumpWidget(const MaterialApp(
+        home: LoginScreen(),
+      ));
+
+      final heading = find.text('EasyNote');
+      final subtitle = find.text('Capture your ideas');
+      final loginButton = find.byType(RoundButton);
+      final textField = find.byType(TextField);
+
+      expect(loginButton, findsOneWidget); // Find login button
+      expect(heading, findsOneWidget); // Find main heading
+      expect(subtitle, findsOneWidget); // find subtitle
+      expect(textField, findsNWidgets(2)); // find 2 textfields
+    });
   });
 }
