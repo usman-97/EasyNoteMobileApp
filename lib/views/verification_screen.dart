28a974f17b0b806bc08ab9013734b2e8173851be
Usman <usmanshabir65@hotmail.com>
2022-01-27 13:41:02 +0000

    shows user email

--- lib/views/verification_screen.dart
+++ lib/views/verification_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:note_taking_app/utilities/constants.dart';
+import 'package:note_taking_app/viewModels/Verification_screen_view_model.dart';
 
 class VerificationScreen extends StatefulWidget {
   static const String id = 'verification_screen';
@@ -11,13 +12,67 @@ class VerificationScreen extends StatefulWidget {
 }
 
 class _VerificationScreenState extends State<VerificationScreen> {
+  final VerificationScreenViewModel _verificationScreenViewModel =
+      VerificationScreenViewModel();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: kDarkPrimaryColour,
+      backgroundColor: kLightPrimaryColour,
       body: SafeArea(
         child: Column(
-          children: <Widget>[],
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              color: kDarkPrimaryColour,
+              height: 250.0,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 10.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text('Hello User'),
+                    TextButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                        backgroundColor: kLightPrimaryColour,
+                      ),
+                      child: const Icon(Icons.exit_to_app_outlined),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            Flexible(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 20.0, vertical: 10.0),
+                  color: kPrimaryColour,
+                  height: 120.0,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Flexible(
+                        child: Text(
+                          'Verification email has been sent to ${_verificationScreenViewModel.getCurrentUserEmail()}. Please verify your email to complete the registration.',
+                          style: const TextStyle(
+                              color: kTextIconColour, fontSize: 20.0),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
