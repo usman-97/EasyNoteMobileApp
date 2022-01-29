230f8fd2bd847245df46a8a8bf5a29820c88c672
Usman <usmanshabir65@hotmail.com>
2022-01-29 02:26:13 +0000

    navigation utility added

---
+++ lib/utilities/navigation.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:note_taking_app/views/home_screen.dart';
+import 'package:note_taking_app/views/login_screen.dart';
+import 'package:note_taking_app/views/register_screen.dart';
+import 'package:note_taking_app/views/verification_screen.dart';
+
+class Navigation {
+  static void navigateToHome(BuildContext context) {
+    Navigator.pushNamed(context, HomeScreen.id);
+  }
+
+  static void navigateToLogin(BuildContext context) {
+    Navigator.pushNamed(context, LoginScreen.id);
+  }
+
+  static void navigateToRegister(BuildContext context) {
+    Navigator.pushNamed(context, RegisterScreen.id);
+  }
+
+  static void navigateToVerification(BuildContext context) {
+    Navigator.pushNamed(context, VerificationScreen.id);
+  }
+}
