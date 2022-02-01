ff79efab43200cde68217417f02ec07e4f92dee8
Usman <usmanshabir65@hotmail.com>
2022-02-01 23:44:08 +0000

    Top widget changed to ListView from Column

--- lib/views/register_screen.dart
+++ lib/views/register_screen.dart
@@ -30,229 +30,235 @@ class _RegisterScreenState extends State<RegisterScreen> {
     return Scaffold(
       backgroundColor: kDarkPrimaryColour,
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(
-              'EasyNote',
-              style: kMainHeadingStyle.copyWith(fontSize: 40.0),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(
-              height: 10.0,
-              child: Divider(
-                thickness: 2.0,
-                color: kTextIconColour,
-                indent: 70.0,
-                endIndent: 70.0,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 50.0),
+          child: ListView(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            // crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              const Text(
+                'EasyNote',
+                style: kMainHeadingStyle,
+                textAlign: TextAlign.center,
               ),
-            ),
-            Text(
-              'Capture your ideas',
-              style: kMainHeadingSubtitle.copyWith(fontSize: 16.0),
-              textAlign: TextAlign.center,
-            ),
-            Visibility(
-              visible: !_isNextSectionVisible,
-              child: Center(
-                child: SizedBox(
-                  width: 350.0,
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                      top: 20.0,
-                    ),
-                    child: TextField(
-                      keyboardType: TextInputType.emailAddress,
-                      textAlign: TextAlign.center,
-                      onChanged: (value) {
-                        _firstname = value;
-                      },
-                      decoration: kTextFieldInputDecoration.copyWith(
-                        fillColor: kTextIconColour,
-                        hintText: 'First name',
+              const SizedBox(
+                height: 10.0,
+                child: Divider(
+                  thickness: 2.0,
+                  color: kTextIconColour,
+                  indent: 70.0,
+                  endIndent: 70.0,
+                ),
+              ),
+              const Text(
+                'Capture your ideas',
+                style: kMainHeadingSubtitle,
+                textAlign: TextAlign.center,
+              ),
+              Visibility(
+                visible: !_isNextSectionVisible,
+                child: Center(
+                  child: SizedBox(
+                    width: 350.0,
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        top: 20.0,
+                      ),
+                      child: TextField(
+                        keyboardType: TextInputType.emailAddress,
+                        textAlign: TextAlign.center,
+                        onChanged: (value) {
+                          _firstname = value;
+                        },
+                        decoration: kTextFieldInputDecoration.copyWith(
+                          fillColor: kTextIconColour,
+                          hintText: 'First name',
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
-            Visibility(
-              visible: !_isNextSectionVisible,
-              child: Center(
-                child: SizedBox(
-                  width: 350.0,
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                      top: 20.0,
-                    ),
-                    child: TextField(
-                      keyboardType: TextInputType.emailAddress,
-                      textAlign: TextAlign.center,
-                      onChanged: (value) {
-                        _lastname = value;
-                      },
-                      decoration: kTextFieldInputDecoration.copyWith(
-                        fillColor: kTextIconColour,
-                        hintText: 'Last name',
+              Visibility(
+                visible: !_isNextSectionVisible,
+                child: Center(
+                  child: SizedBox(
+                    width: 350.0,
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        top: 20.0,
+                      ),
+                      child: TextField(
+                        keyboardType: TextInputType.emailAddress,
+                        textAlign: TextAlign.center,
+                        onChanged: (value) {
+                          _lastname = value;
+                        },
+                        decoration: kTextFieldInputDecoration.copyWith(
+                          fillColor: kTextIconColour,
+                          hintText: 'Last name',
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
-            Visibility(
-              visible: _isNextSectionVisible,
-              child: Center(
-                child: SizedBox(
-                  width: 350.0,
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                      top: 20.0,
-                    ),
-                    child: TextField(
-                      keyboardType: TextInputType.emailAddress,
-                      textAlign: TextAlign.center,
-                      onChanged: (value) {
-                        _email = value;
-                      },
-                      decoration: kTextFieldInputDecoration.copyWith(
-                        fillColor: kTextIconColour,
-                        hintText: 'Email',
+              Visibility(
+                visible: _isNextSectionVisible,
+                child: Center(
+                  child: SizedBox(
+                    width: 350.0,
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        top: 20.0,
+                      ),
+                      child: TextField(
+                        keyboardType: TextInputType.emailAddress,
+                        textAlign: TextAlign.center,
+                        onChanged: (value) {
+                          _email = value;
+                        },
+                        decoration: kTextFieldInputDecoration.copyWith(
+                          fillColor: kTextIconColour,
+                          hintText: 'Email',
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
-            Visibility(
-              visible: _isNextSectionVisible,
-              child: Center(
-                child: SizedBox(
-                  width: 350.0,
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                      top: 10.0,
-                    ),
-                    child: TextField(
-                      obscureText: true,
-                      textAlign: TextAlign.center,
-                      onChanged: (value) {
-                        _password = value;
-                      },
-                      decoration: kTextFieldInputDecoration.copyWith(
-                        fillColor: kTextIconColour,
-                        hintText: 'Password',
+              Visibility(
+                visible: _isNextSectionVisible,
+                child: Center(
+                  child: SizedBox(
+                    width: 350.0,
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        top: 10.0,
+                      ),
+                      child: TextField(
+                        obscureText: true,
+                        textAlign: TextAlign.center,
+                        onChanged: (value) {
+                          _password = value;
+                        },
+                        decoration: kTextFieldInputDecoration.copyWith(
+                          fillColor: kTextIconColour,
+                          hintText: 'Password',
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
-            Visibility(
-              visible: _isNextSectionVisible,
-              child: Center(
-                child: SizedBox(
-                  width: 350.0,
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                      top: 10.0,
-                    ),
-                    child: TextField(
-                      obscureText: true,
-                      textAlign: TextAlign.center,
-                      onChanged: (value) {
-                        _confirmPassword = value;
-                      },
-                      decoration: kTextFieldInputDecoration.copyWith(
-                        fillColor: kTextIconColour,
-                        hintText: 'Confirm Password',
+              Visibility(
+                visible: _isNextSectionVisible,
+                child: Center(
+                  child: SizedBox(
+                    width: 350.0,
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        top: 10.0,
+                      ),
+                      child: TextField(
+                        obscureText: true,
+                        textAlign: TextAlign.center,
+                        onChanged: (value) {
+                          _confirmPassword = value;
+                        },
+                        decoration: kTextFieldInputDecoration.copyWith(
+                          fillColor: kTextIconColour,
+                          hintText: 'Confirm Password',
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                child: Text(
-                  _registrationError,
-                  style: kErrorMessageStyle,
-                ),
+              const SizedBox(
+                height: 5.0,
               ),
-            ),
-            Visibility(
-              visible: !_isNextSectionVisible,
-              child: Center(
-                child: RoundButton(
-                  label: 'Next',
-                  onPressed: () {
-                    setState(() {
-                      _isNextSectionVisible =
-                          _isNextSectionVisible == true ? false : true;
-                    });
-                  },
-                ),
-              ),
-            ),
-            Visibility(
-              visible: _isNextSectionVisible,
-              child: Center(
-                child: RoundButton(
-                  label: 'Register',
-                  onPressed: () async {
-                    await _registerScreenViewModel.registerUser(
-                        _email, _password, _confirmPassword);
-                    setState(() {
-                      _registrationError =
-                          _registerScreenViewModel.getRegistrationError();
-                      if (_registrationError.isEmpty) {
-                        _registerScreenViewModel.addUserData(
-                            _email, _firstname, _lastname);
-                        // _registerScreenViewModel.navigateToVerificationScreen(context);
-                        Navigation.navigateToVerification(context);
-                      }
-                      // print(_registerScreenViewModel.getCurrentUser());
-                    });
-                  },
-                ),
-              ),
-            ),
-            Visibility(
-              visible: _isNextSectionVisible,
-              child: Center(
-                child: RoundButton(
-                  label: 'Back',
-                  onPressed: () {
-                    setState(() {
-                      _isNextSectionVisible =
-                          _isNextSectionVisible == true ? false : true;
-                    });
-                  },
-                ),
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  'Already registered?',
-                  style: TextStyle(
-                    color: kTextIconColour,
-                    fontSize: 18.0,
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Text(
+                    _registrationError,
+                    style: kErrorMessageStyle,
                   ),
                 ),
-                LinkButton(
-                  text: 'Login here',
-                  onPressed: () {
-                    // _registerScreenViewModel.navigateToLoginScreen(context);
-                    Navigation.navigateToLogin(context);
-                  },
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text(
+                    'Already registered?',
+                    style: TextStyle(
+                      color: kTextIconColour,
+                      fontSize: 18.0,
+                    ),
+                  ),
+                  LinkButton(
+                    text: 'Login here',
+                    onPressed: () {
+                      // _registerScreenViewModel.navigateToLoginScreen(context);
+                      Navigation.navigateToLogin(context);
+                    },
+                  ),
+                ],
+              ),
+              Visibility(
+                visible: !_isNextSectionVisible,
+                child: Center(
+                  child: RoundButton(
+                    label: 'Next',
+                    onPressed: () {
+                      setState(() {
+                        _isNextSectionVisible =
+                            _isNextSectionVisible == true ? false : true;
+                      });
+                    },
+                  ),
                 ),
-              ],
-            ),
-          ],
+              ),
+              Visibility(
+                visible: _isNextSectionVisible,
+                child: Center(
+                  child: RoundButton(
+                    label: 'Register',
+                    onPressed: () async {
+                      await _registerScreenViewModel.registerUser(
+                          _email, _password, _confirmPassword);
+                      setState(() {
+                        _registrationError =
+                            _registerScreenViewModel.getRegistrationError();
+                        if (_registerScreenViewModel.isUserRegistered()) {
+                          _registerScreenViewModel.addUserData(
+                              _email, _firstname, _lastname);
+                          // _registerScreenViewModel.navigateToVerificationScreen(context);
+                          Navigation.navigateToVerification(context);
+                        }
+                        // print(_registerScreenViewModel.getCurrentUser());
+                      });
+                    },
+                  ),
+                ),
+              ),
+              Visibility(
+                visible: _isNextSectionVisible,
+                child: Center(
+                  child: RoundButton(
+                    label: 'Back',
+                    onPressed: () {
+                      setState(() {
+                        _isNextSectionVisible =
+                            _isNextSectionVisible == true ? false : true;
+                      });
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
