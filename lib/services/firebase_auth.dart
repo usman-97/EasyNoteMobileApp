8d647561a13f8366d86145cf297df12e2d572de2
Usman <usmanshabir65@hotmail.com>
2022-02-04 22:21:22 +0000

    userChange service added

--- lib/services/firebase_auth.dart
+++ lib/services/firebase_auth.dart
@@ -1,9 +1,14 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 class FirebaseAuthentication {
+  static final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  static final Stream<User?> _userChange = _firebaseAuth.userChanges();
+
   FirebaseAuthentication._();
 
   static FirebaseAuth firebaseAuthInstance() {
-    return FirebaseAuth.instance;
+    return _firebaseAuth;
   }
+
+  static Stream<User?> get userChange => _userChange;
 }
