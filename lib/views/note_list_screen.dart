fac035af837c09e4feab784047c5817c6b10a874
Usman <usmanshabir65@hotmail.com>
2022-02-04 19:10:06 +0000

    note card widget added

---
+++ lib/views/note_list_screen.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:note_taking_app/components/add_button.dart';
+import 'package:note_taking_app/components/app_menu.dart';
+import 'package:note_taking_app/components/custom_app_bar.dart';
+import 'package:note_taking_app/components/noteCard.dart';
+import 'package:note_taking_app/utilities/constants.dart';
+
+class NoteListScreen extends StatefulWidget {
+  static const String id = 'note_list_screen';
+
+  const NoteListScreen({Key? key}) : super(key: key);
+
+  @override
+  _NoteListScreenState createState() => _NoteListScreenState();
+}
+
+class _NoteListScreenState extends State<NoteListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: const <Widget>[
+          CustomAppBar(),
+        ],
+      ),
+      drawer: AppMenu(),
+      backgroundColor: kLightPrimaryColour,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              color: kPrimaryColour,
+              height: 150.0,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Padding(
+                      padding: EdgeInsets.only(bottom: 10.0),
+                      child: Icon(
+                        Icons.arrow_back_rounded,
+                        color: kTextIconColour,
+                        size: 40.0,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 20.0,
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(bottom: 15.0),
+                    child: Text(
+                      'Notes',
+                      style: TextStyle(
+                        color: kTextIconColour,
+                        fontSize: 40.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Stack(
+                children: [
+                  ListView(
+                    children: const <Widget>[
+                      NoteCard(),
+                      NoteCard(),
+                      NoteCard(),
+                      NoteCard(),
+                      NoteCard(),
+                    ],
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(bottom: 20.0),
+                    child: Align(
+                      alignment: FractionalOffset.bottomCenter,
+                      child: AddButton(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
