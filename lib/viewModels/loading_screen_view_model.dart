63b5a5cf4733449f3fe30a5532606c5febef6172
Usman <usmanshabir65@hotmail.com>
2022-02-04 00:10:35 +0000

    navigateToLoginScreen method changed to appLoading

--- lib/viewModels/loading_screen_view_model.dart
+++ lib/viewModels/loading_screen_view_model.dart
@@ -1,10 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:note_taking_app/models/user_authentication.dart';
+import 'package:note_taking_app/views/home_screen.dart';
 import 'package:note_taking_app/views/login_screen.dart';
 
 class LoadingScreenViewModel {
-  void navigateToLoginScreen(BuildContext context) {
+  UserAuthentication _userAuthentication = UserAuthentication();
+
+  void appLoading(BuildContext context) {
     Future.delayed(const Duration(seconds: 2, milliseconds: 500), () {
-      Navigator.pushNamed(context, LoginScreen.id);
+      bool isUserSignedIn = _userAuthentication.isUserAlreadySignedIn();
+      if (isUserSignedIn) {
+        Navigator.pushNamed(context, HomeScreen.id);
+      } else {
+        Navigator.pushNamed(context, LoginScreen.id);
+      }
     });
   }
 }
