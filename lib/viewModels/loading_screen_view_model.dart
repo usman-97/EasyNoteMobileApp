1870435419fc5c61bba6dcb23b0d500682ede812
Usman <usmanshabir65@hotmail.com>
2022-02-04 22:21:54 +0000

    appLoading method modified

--- lib/viewModels/loading_screen_view_model.dart
+++ lib/viewModels/loading_screen_view_model.dart
@@ -1,19 +1,21 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:note_taking_app/models/user_authentication.dart';
 import 'package:note_taking_app/views/home_screen.dart';
 import 'package:note_taking_app/views/login_screen.dart';
 
 class LoadingScreenViewModel {
-  UserAuthentication _userAuthentication = UserAuthentication();
+  final UserAuthentication _userAuthentication = UserAuthentication();
 
   void appLoading(BuildContext context) {
     Future.delayed(const Duration(seconds: 2, milliseconds: 500), () {
-      bool isUserSignedIn = _userAuthentication.isUserAlreadySignedIn();
-      if (isUserSignedIn) {
-        Navigator.pushNamed(context, HomeScreen.id);
-      } else {
+      if (_userAuthentication.getCurrentUser() == null) {
         Navigator.pushNamed(context, LoginScreen.id);
+        // print('User has not been Signed in');
+      } else {
+        Navigator.pushNamed(context, HomeScreen.id);
+        // print('User has been Signed in');
       }
     });
   }
