5e0eaaad64cc64ab943d7e89bb79d452f4774bd3
Usman <usmanshabir65@hotmail.com>
2022-02-05 01:32:47 +0000

    create_note_screen added

---
+++ lib/views/create_note_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CreateNoteScreen extends StatefulWidget {
+  static const String id = 'create_note_screen';
+
+  const CreateNoteScreen({Key? key}) : super(key: key);
+
+  @override
+  _CreateNoteScreenState createState() => _CreateNoteScreenState();
+}
+
+class _CreateNoteScreenState extends State<CreateNoteScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
