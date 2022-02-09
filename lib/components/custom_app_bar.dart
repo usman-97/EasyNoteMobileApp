cfef75349f090e293dbc4ec0f978a064642d8a12
Usman <usmanshabir65@hotmail.com>
2022-02-09 03:21:03 +0000

    initial commit

--- lib/components/custom_app_bar.dart
+++ lib/components/custom_app_bar.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:note_taking_app/utilities/constants.dart';
 
 class CustomAppBar extends StatelessWidget {
+  final bool isEditNote;
+
   const CustomAppBar({
+    this.isEditNote = false,
     Key? key,
   }) : super(key: key);
 
@@ -10,10 +13,12 @@ class CustomAppBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.only(right: 10.0),
-      child: Icon(
-        Icons.search_rounded,
-        color: kTextIconColour,
-        size: 40.0,
+      child: Visibility(
+        child: Icon(
+          Icons.search_rounded,
+          color: kTextIconColour,
+          size: 40.0,
+        ),
       ),
     );
   }
--- lib/views/create_note_screen.dart
+++ lib/views/create_note_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:note_taking_app/components/app_menu.dart';
+import 'package:note_taking_app/components/custom_app_bar.dart';
+import 'package:flutter_quill/flutter_quill.dart';
+import 'package:note_taking_app/utilities/constants.dart';
+import 'package:note_taking_app/viewModels/create_note_view_model.dart';
 
 class CreateNoteScreen extends StatefulWidget {
   static const String id = 'create_note_screen';
@@ -10,8 +15,70 @@ class CreateNoteScreen extends StatefulWidget {
 }
 
 class _CreateNoteScreenState extends State<CreateNoteScreen> {
+  final QuillController _quillController = QuillController.basic();
+  final CreateNoteViewModel _createNoteViewModel = CreateNoteViewModel();
+  bool _isEditNote = false;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        leading: TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Visibility(
+            visible: !_isEditNote,
+            child: const Icon(
+              Icons.arrow_back_rounded,
+              size: 40.0,
+              color: kTextIconColour,
+            ),
+          ),
+        ),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {
+              setState(() {
+                _isEditNote = _isEditNote ? false : true;
+                print(_isEditNote);
+              });
+            },
+            child: Icon(
+              _isEditNote ? Icons.check_rounded : Icons.edit_rounded,
+              color: kTextIconColour,
+              size: 40.0,
+            ),
+          )
+        ],
+      ),
+      // drawer: AppMenu(),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Visibility(
+              visible: _isEditNote,
+              child: QuillToolbar.basic(
+                controller: _quillController,
+                onImagePickCallback: _createNoteViewModel.onImagePickCallback,
+                onVideoPickCallback: _createNoteViewModel.onVideoPickCallBack,
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 10.0,
+                  vertical: 15.0,
+                ),
+                child: QuillEditor.basic(
+                  controller: _quillController,
+                  readOnly: !_isEditNote,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
