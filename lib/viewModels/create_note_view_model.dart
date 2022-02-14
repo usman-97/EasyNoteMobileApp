5d80f8adf92202267bb7aa948281506c2ebc3408
Usman <usmanshabir65@hotmail.com>
2022-02-14 21:44:16 +0000

    File picker method added

--- lib/viewModels/create_note_view_model.dart
+++ lib/viewModels/create_note_view_model.dart
@@ -4,17 +4,17 @@ import 'package:image_picker/image_picker.dart';
 import 'package:camera/camera.dart';
 
 class CreateNoteViewModel {
-  // final ImagePicker _imagePicker = ImagePicker();
-  //
-  // Future<String> onImagePickCallback(File file) async {
-  //   final XFile? image =
-  //       await _imagePicker.pickImage(source: ImageSource.gallery);
-  //   return image!.path;
-  // }
-  //
-  // Future<String> onVideoPickCallBack(File file) async {
-  //   final XFile? video =
-  //       await _imagePicker.pickVideo(source: ImageSource.gallery);
-  //   return video!.path;
-  // }
+  final ImagePicker _imagePicker = ImagePicker();
+
+  Future<String> onImagePickCallback(File file) async {
+    final XFile? image =
+        await _imagePicker.pickImage(source: ImageSource.gallery);
+    return image!.path;
+  }
+
+  Future<String> onVideoPickCallBack(File file) async {
+    final XFile? video =
+        await _imagePicker.pickVideo(source: ImageSource.gallery);
+    return video!.path;
+  }
 }
