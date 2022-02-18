a4e016c14c8718e923505ea1f4ce0c3c164c9650
Usman <usmanshabir65@hotmail.com>
2022-02-18 23:52:58 +0000

    Note is added to Firestore when it is saved by user

--- lib/viewModels/create_note_view_model.dart
+++ lib/viewModels/create_note_view_model.dart
@@ -1,10 +1,15 @@
+import 'dart:convert';
 import 'dart:io';
-import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:camera/camera.dart';
+import 'package:note_taking_app/models/note_firestore.dart';
+import 'package:note_taking_app/models/note_storage.dart';
 
 class CreateNoteViewModel {
   final ImagePicker _imagePicker = ImagePicker();
+  final NoteStorage _noteStorage = NoteStorage();
+  final NoteFireStore _noteFireStore = NoteFireStore();
+  String _error = '';
 
   Future<String> onImagePickCallback(File file) async {
     final XFile? image =
@@ -17,4 +22,31 @@ class CreateNoteViewModel {
         await _imagePicker.pickVideo(source: ImageSource.gallery);
     return video!.path;
   }
+
+  Future<void> uploadNoteToCloud(Object obj, String filename) async {
+    var jsonFile = jsonEncode(obj);
+    bool isUploaded = await _noteStorage.uploadFileToCloud(jsonFile, filename);
+    if (!isUploaded) {
+      _error = 'File not uploaded';
+    }
+  }
+
+  Future<void> addNoteToFirestore(String noteTitle) async {
+    String date = _getDate();
+    print(date);
+    bool isDocExist = await _noteFireStore.isNotesDocumentExist(noteTitle);
+    if (isDocExist) {
+      await _noteFireStore.updateLastModified(noteTitle, date);
+    } else {
+      await _noteFireStore.addNote(noteTitle, date);
+    }
+  }
+
+  String _getDate() {
+    DateTime now = DateTime.now();
+    DateTime date = DateTime(now.day, now.month, now.year);
+    return date.toString();
+  }
+
+  String get getError => _error;
 }
