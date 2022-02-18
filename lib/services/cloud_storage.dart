524f072f0993d0ccea40c04df6fbefc4039babbc
Usman <usmanshabir65@hotmail.com>
2022-02-18 21:51:14 +0000

    Firebase Cloud service added

--- lib/services/cloud_storage.dart
+++ lib/services/cloud_storage.dart
@@ -3,7 +3,13 @@ import 'package:firebase_storage/firebase_storage.dart';
 class CloudStorage {
   CloudStorage._();
 
-  FirebaseStorage firebaseStorageInstance() {
-    return FirebaseStorage.instance;
+  static final FirebaseStorage _firebaseStorage = FirebaseStorage.instance;
+
+  static FirebaseStorage firebaseStorageInstance() {
+    return _firebaseStorage;
+  }
+
+  static Reference fileReference({String? path}) {
+    return _firebaseStorage.ref(path);
   }
 }
