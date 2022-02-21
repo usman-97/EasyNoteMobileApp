4ca79f65ec4657c857f69d94a598a768bae95e40
Usman <usmanshabir65@hotmail.com>
2022-02-21 23:05:13 +0000

    Provider widget removed

--- lib/views/note_list_screen.dart
+++ lib/views/note_list_screen.dart
@@ -24,102 +24,96 @@ class _NoteListScreenState extends State<NoteListScreen> {
   @override
   Widget build(BuildContext context) {
     // _noteListViewModel.fetchAllUserNotes();
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => NoteListViewModel()),
-      ],
-      child: Scaffold(
-        appBar: AppBar(
-          actions: const <Widget>[
-            CustomAppBar(),
+    return Scaffold(
+      appBar: AppBar(
+        actions: const <Widget>[
+          CustomAppBar(),
+        ],
+      ),
+      drawer: AppMenu(),
+      backgroundColor: kLightPrimaryColour,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              color: kPrimaryColour,
+              height: 150.0,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, HomeScreen.id);
+                    },
+                    child: const Padding(
+                      padding: EdgeInsets.only(bottom: 10.0),
+                      child: Icon(
+                        Icons.arrow_back_rounded,
+                        color: kTextIconColour,
+                        size: 40.0,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 20.0,
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(bottom: 15.0),
+                    child: Text(
+                      'Notes',
+                      style: TextStyle(
+                        color: kTextIconColour,
+                        fontSize: 40.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Stack(
+                children: [
+                  StreamBuilder(
+                    stream: _noteListViewModel.fetchAllUserNotes(),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<dynamic> snapshot) {
+                      // final notesData = snapshot.data;
+                      // List<NoteCard> userNoteCards = [];
+                      // for (var noteData in notesData) {
+                      //   userNoteCards.add(NoteCard(
+                      //       title: noteData.note_title,
+                      //       date_created: noteData.date_created,
+                      //       last_modified: noteData.last_modified,
+                      //       status: noteData.status));
+                      // }
+
+                      if (!snapshot.hasData) {
+                        return Container();
+                      }
+
+                      _noteListViewModel.buildUserNoteCards(snapshot, context);
+                      return ListView(
+                        children: _noteListViewModel.userNoteCards,
+                      );
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 20.0),
+                    child: Align(
+                      alignment: FractionalOffset.bottomCenter,
+                      child: CircleButton(
+                        onPressed: () {
+                          Navigation.navigateToCreateNote(context);
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
-        ),
-        drawer: AppMenu(),
-        backgroundColor: kLightPrimaryColour,
-        body: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                color: kPrimaryColour,
-                height: 150.0,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: <Widget>[
-                    TextButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, HomeScreen.id);
-                      },
-                      child: const Padding(
-                        padding: EdgeInsets.only(bottom: 10.0),
-                        child: Icon(
-                          Icons.arrow_back_rounded,
-                          color: kTextIconColour,
-                          size: 40.0,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 20.0,
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.only(bottom: 15.0),
-                      child: Text(
-                        'Notes',
-                        style: TextStyle(
-                          color: kTextIconColour,
-                          fontSize: 40.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Expanded(
-                child: Stack(
-                  children: [
-                    StreamBuilder(
-                      stream: _noteListViewModel.fetchAllUserNotes(),
-                      builder: (BuildContext context,
-                          AsyncSnapshot<dynamic> snapshot) {
-                        // final notesData = snapshot.data;
-                        // List<NoteCard> userNoteCards = [];
-                        // for (var noteData in notesData) {
-                        //   userNoteCards.add(NoteCard(
-                        //       title: noteData.note_title,
-                        //       date_created: noteData.date_created,
-                        //       last_modified: noteData.last_modified,
-                        //       status: noteData.status));
-                        // }
-
-                        if (!snapshot.hasData) {
-                          return Container();
-                        }
-
-                        _noteListViewModel.buildUserNoteCards(
-                            snapshot, context);
-                        return ListView(
-                          children: _noteListViewModel.userNoteCards,
-                        );
-                      },
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 20.0),
-                      child: Align(
-                        alignment: FractionalOffset.bottomCenter,
-                        child: CircleButton(
-                          onPressed: () {
-                            Navigation.navigateToCreateNote(context);
-                          },
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
         ),
       ),
     );
