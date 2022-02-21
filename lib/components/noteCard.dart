c1ec1bb0470312f365a2093bbf28c16b5f14ae0b
Usman <usmanshabir65@hotmail.com>
2022-02-21 17:52:25 +0000

    Gesture widget added at the top level widget

--- lib/components/noteCard.dart
+++ lib/components/noteCard.dart
@@ -3,39 +3,50 @@ import 'package:flutter/material.dart';
 class NoteCard extends StatelessWidget {
   const NoteCard({
     Key? key,
+    required this.title,
+    required this.date_created,
+    required this.last_modified,
+    required this.status,
   }) : super(key: key);
+
+  final String title, date_created, last_modified, status;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
-      child: Card(
-        color: Colors.amberAccent,
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              const Text(
-                'Title',
-                style: TextStyle(fontSize: 40.0),
-              ),
-              const Text(
-                'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s',
-                style: TextStyle(fontSize: 14.0),
-              ),
-              const SizedBox(
-                height: 10.0,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: const <Widget>[
-                  Text('07/03/2022'),
-                  Text('Category'),
-                ],
-              ),
-            ],
+    return GestureDetector(
+      onTap: () {
+        print(title);
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+        child: Card(
+          color: Colors.amberAccent,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Text(
+                  title,
+                  style: TextStyle(fontSize: 40.0),
+                ),
+                // const Text(
+                //   'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s',
+                //   style: TextStyle(fontSize: 14.0),
+                // ),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    Text('Last Modified: $last_modified'),
+                    Text(status),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
