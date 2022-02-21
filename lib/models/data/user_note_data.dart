58ca9b2131520fe0edaf3c36de86ae9fa0ad7df9
Usman <usmanshabir65@hotmail.com>
2022-02-21 17:44:47 +0000

    Data model added for user note data

--- lib/models/data/user_note_data.dart
+++ lib/models/data/user_note_data.dart
@@ -1,23 +1,66 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class UserNoteData {
-  late String _documentID, _note_title, _date_created, _last_modified, _status;
+  final String documentID, note_title, date_created, last_modified, status;
+  UserNoteData(
+      {required this.documentID,
+      required this.note_title,
+      required this.date_created,
+      required this.last_modified,
+      required this.status});
 
-  UserNoteData(QueryDocumentSnapshot<Map<String, dynamic>> data) {
-    _documentID = data.get('id');
-    _note_title = data.get('title');
-    _date_created = data.get('date_created');
-    _last_modified = data.get('last_modified');
-    _status = data.get('status');
-  }
+  UserNoteData.fromDocumentSnapshot(Map<String, dynamic> jsonData)
+      : documentID = jsonData['id'],
+        note_title = jsonData['title'],
+        date_created = jsonData['date_created'],
+        last_modified = jsonData['last_modified'],
+        status = jsonData['status'];
 
-  get status => _status;
+  Map<String, dynamic> toJson() => {
+        'id': documentID,
+        'title': note_title,
+        'date_created': date_created,
+        'last_modified': last_modified,
+        'status': status,
+      };
 
-  get last_modified => _last_modified;
+  // Map<String, dynamic> toMap() {
+  //   return {
+  //     'id': documentID,
+  //     'title': note_title,
+  //     'date_created': date_created,
+  //     'last_modified': last_modified,
+  //     'status': status,
+  //   };
+  // }
+  //
+  // static fromMap(Map<String, dynamic> map) {
+  //   if (map == null) return null;
+  //
+  //   return UserNoteData(
+  //     documentID: map['id'],
+  //     note_title: map['note_title'],
+  //     date_created: map['date_created'],
+  //     last_modified: map['last_modified'],
+  //     status: map['status'],
+  //   );
+  // }
 
-  get date_created => _date_created;
-
-  get note_title => _note_title;
-
-  String get documentID => _documentID;
+  // UserNoteData(QueryDocumentSnapshot<Map<String, dynamic>> data) {
+  //   _documentID = data.get('id');
+  //   _note_title = data.get('title');
+  //   _date_created = data.get('date_created');
+  //   _last_modified = data.get('last_modified');
+  //   _status = data.get('status');
+  // }
+  //
+  // get status => _status;
+  //
+  // get last_modified => _last_modified;
+  //
+  // get date_created => _date_created;
+  //
+  // get note_title => _note_title;
+  //
+  // String get documentID => _documentID;
 }
