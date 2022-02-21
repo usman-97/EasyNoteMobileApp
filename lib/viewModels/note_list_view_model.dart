b657b793b6c51c76638d05afbc1af5d5523d7790
Usman <usmanshabir65@hotmail.com>
2022-02-21 23:00:13 +0000

    Navigate to CreateNoteScreen on NoteCard tap

--- lib/viewModels/note_list_view_model.dart
+++ lib/viewModels/note_list_view_model.dart
@@ -1,9 +1,12 @@
 import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:note_taking_app/components/noteCard.dart';
 import 'package:note_taking_app/models/data/user_note_data.dart';
 import 'package:note_taking_app/models/user_note.dart';
+import 'package:note_taking_app/utilities/navigation.dart';
+import 'package:note_taking_app/views/create_note_screen.dart';
 
 class NoteListViewModel with ChangeNotifier {
   final UserNote _userNote = UserNote();
@@ -15,15 +18,25 @@ class NoteListViewModel with ChangeNotifier {
     return _userNote.fetchAllUserNoteData();
   }
 
-  void buildUserNoteCards(AsyncSnapshot<dynamic> snapshot) {
+  void buildUserNoteCards(
+      AsyncSnapshot<dynamic> snapshot, BuildContext context) {
     final notesData = snapshot.data;
     // List<NoteCard> userNoteCards = [];
     for (var noteData in notesData) {
       _userNoteCards.add(NoteCard(
-          title: noteData.note_title,
-          date_created: noteData.date_created,
-          last_modified: noteData.last_modified,
-          status: noteData.status));
+        title: noteData.note_title,
+        date_created: noteData.date_created,
+        last_modified: noteData.last_modified,
+        status: noteData.status,
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return CreateNoteScreen(
+              isEditable: false,
+              documentID: noteData.documentID,
+            );
+          }));
+        },
+      ));
     }
   }
 }
