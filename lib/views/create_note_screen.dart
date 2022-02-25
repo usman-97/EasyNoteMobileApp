083bbee422299e518e3c796aa00a6927516d93e5
Usman <usmanshabir65@hotmail.com>
2022-02-25 19:35:40 +0000

    user can now edit thier saved notes

--- lib/views/create_note_screen.dart
+++ lib/views/create_note_screen.dart
@@ -1,50 +1,53 @@
-import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:note_taking_app/components/app_menu.dart';
 import 'package:note_taking_app/components/circle_button.dart';
-import 'package:note_taking_app/components/custom_app_bar.dart';
-import 'package:note_taking_app/components/custom_editor_button.dart';
 import 'package:note_taking_app/utilities/constants.dart';
 import 'package:note_taking_app/utilities/navigation.dart';
 import 'package:note_taking_app/viewModels/create_note_view_model.dart';
 import 'package:flutter_quill/flutter_quill.dart';
 
-// import 'package:html_editor_enhanced/html_editor.dart';
-// import 'package:html_editor_enhanced/utils/callbacks.dart';
-// import 'package:html_editor_enhanced/utils/file_upload_model.dart';
-// import 'package:html_editor_enhanced/utils/options.dart';
-// import 'package:html_editor_enhanced/utils/plugins.dart';
-// import 'package:html_editor_enhanced/utils/shims/dart_ui.dart';
-// import 'package:html_editor_enhanced/utils/shims/dart_ui_fake.dart';
-// import 'package:html_editor_enhanced/utils/shims/dart_ui_real.dart';
-// import 'package:html_editor_enhanced/utils/shims/flutter_inappwebview_fake.dart';
-// import 'package:html_editor_enhanced/utils/toolbar.dart';
-// import 'package:html_editor_enhanced/utils/utils.dart';
-
 class CreateNoteScreen extends StatefulWidget {
   static const String id = 'create_note_screen';
   final bool isEditable;
-  // final CameraDescription cameraDescription;
+  final String documentID, title;
 
-  const CreateNoteScreen({Key? key, this.isEditable = true}) : super(key: key);
+  const CreateNoteScreen({
+    Key? key,
+    this.documentID = '',
+    this.title = 'Untitled',
+    this.isEditable = true,
+  }) : super(key: key);
 
   @override
   _CreateNoteScreenState createState() => _CreateNoteScreenState();
 }
 
 class _CreateNoteScreenState extends State<CreateNoteScreen> {
-  final QuillController _quillController = QuillController.basic();
+  QuillController _quillController = QuillController.basic();
   final CreateNoteViewModel _createNoteViewModel = CreateNoteViewModel();
   final FocusNode _focusNode = FocusNode();
   final TextEditingController _titleTextFieldController =
       TextEditingController();
-  late bool _isEditNote;
+  late bool _isEditNote, _isToolBarExpaneded = false;
+  late String documentID, title;
 
   @override
   void initState() {
     super.initState();
-    _titleTextFieldController.text = 'Untitled';
+    _titleTextFieldController.text = widget.title;
     _isEditNote = widget.isEditable;
+    documentID = widget.documentID;
+    _loadDoc(documentID);
+  }
+
+  void _loadDoc(String filename) async {
+    final doc = await _createNoteViewModel.downloadNoteFromCloud(filename);
+    if (filename.isNotEmpty) {
+      setState(() {
+        _quillController = QuillController(
+            document: doc,
+            selection: const TextSelection(baseOffset: 0, extentOffset: 0));
+      });
+    }
   }
 
   @override
@@ -62,11 +65,25 @@ class _CreateNoteScreenState extends State<CreateNoteScreen> {
             if (!_isEditNote) {
               Navigation.navigateToNoteList(context);
             } else {
+              // String noteDoc = await _createNoteViewModel.generateDocumentName(
+              //         documentID, _titleTextFieldController.text),
+              //     id = await _createNoteViewModel.generateDocumentID(
+              //         documentID, _titleTextFieldController.text);
+              // int totalNotes = await _createNoteViewModel.getTotalNotes();
+              // if (documentID.isEmpty) {
+              //   noteDoc =
+              //       '${_titleTextFieldController.text}${totalNotes + 1}.json';
+              //   id = _titleTextFieldController.text;
+              // } else {
+              //   noteDoc = '$documentID.json';
+              //   id = documentID;
+              // }
               var document = _quillController.document.toDelta().toJson();
-              // await _createNoteViewModel.uploadNoteToCloud(
-              //     document, 'filename');
-              await _createNoteViewModel
-                  .addNoteToFirestore(_titleTextFieldController.text);
+              await _createNoteViewModel.uploadNoteToCloud(
+                  document, documentID, _titleTextFieldController.text);
+              // await _createNoteViewModel.uploadNoteToCloud(document, noteDoc);
+              await _createNoteViewModel.addNote(
+                  documentID, _titleTextFieldController.text);
               setState(() {
                 _isEditNote = false;
                 FocusManager.instance.primaryFocus?.unfocus();
@@ -93,94 +110,33 @@ class _CreateNoteScreenState extends State<CreateNoteScreen> {
       body: SafeArea(
         child: Column(
           children: <Widget>[
-            // Expanded(
-            //   child: Stack(
-            //     children: <Widget>[
-            //       HtmlEditor(
-            //         controller: _htmlEditorController,
-            //         htmlEditorOptions: const HtmlEditorOptions(
-            //           hint: 'Your text here...',
-            //           shouldEnsureVisible: true,
-            //           spellCheck: true,
-            //         ),
-            //         otherOptions: const OtherOptions(
-            //           height: 1200,
-            //         ),
-            //         htmlToolbarOptions: HtmlToolbarOptions(
-            //           toolbarType: ToolbarType.nativeExpandable,
-            //           customToolbarButtons: <Widget>[
-            //             //   TextButton(
-            //             //     onPressed: () {
-            //             //       setState(() {
-            //             //         _isCameraShown = true;
-            //             //       });
-            //             //     },
-            //             //     child: const Icon(
-            //             //       Icons.camera_enhance_rounded,
-            //             //       color: Colors.black87,
-            //             //     ),
-            //             //   ),
-            //             Row(
-            //               children: <Widget>[
-            //                 CustomEditorButton(
-            //                   icon: Icons.undo_rounded,
-            //                   onPressed: () {
-            //                     _htmlEditorController.undo();
-            //                   },
-            //                 ),
-            //                 CustomEditorButton(
-            //                   icon: Icons.redo_rounded,
-            //                   onPressed: () {
-            //                     _htmlEditorController.redo();
-            //                   },
-            //                 ),
-            //               ],
-            //             )
-            //           ],
-            //           defaultToolbarButtons: [
-            //             const StyleButtons(),
-            //             const FontSettingButtons(
-            //               fontSizeUnit: false,
-            //             ),
-            //             const ColorButtons(),
-            //             const ListButtons(listStyles: false),
-            //             const ParagraphButtons(
-            //               lineHeight: false,
-            //               caseConverter: false,
-            //             ),
-            //             const InsertButtons(),
-            //           ],
-            //         ),
-            //       ),
-            //       Padding(
-            //         padding: const EdgeInsets.only(bottom: 20.0),
-            //         child: Visibility(
-            //           visible: !_isEditNote,
-            //           child: Align(
-            //             alignment: FractionalOffset.bottomCenter,
-            //             child: CircleButton(
-            //               icon: Icons.edit_rounded,
-            //               onPressed: () {
-            //                 setState(() {
-            //                   _htmlEditorController.enable();
-            //                   _isEditNote = true;
-            //                 });
-            //               },
-            //             ),
-            //           ),
-            //         ),
-            //       ),
-            //     ],
-            //   ),
-            // ),
-
             Visibility(
               visible: _isEditNote,
-              child: QuillToolbar.basic(
-                controller: _quillController,
-                onImagePickCallback: _createNoteViewModel.onImagePickCallback,
-                onVideoPickCallback: _createNoteViewModel.onVideoPickCallBack,
-                showAlignmentButtons: true,
+              child: Column(
+                children: <Widget>[
+                  QuillToolbar.basic(
+                    controller: _quillController,
+                    onImagePickCallback:
+                        _createNoteViewModel.onImagePickCallback,
+                    onVideoPickCallback:
+                        _createNoteViewModel.onVideoPickCallBack,
+                    showAlignmentButtons: true,
+                    multiRowsDisplay: _isToolBarExpaneded,
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        _isToolBarExpaneded =
+                            _isToolBarExpaneded ? false : true;
+                      });
+                    },
+                    child: Icon(
+                      _isToolBarExpaneded
+                          ? Icons.arrow_upward_rounded
+                          : Icons.arrow_downward_rounded,
+                    ),
+                  ),
+                ],
               ),
             ),
             Expanded(
@@ -219,84 +175,6 @@ class _CreateNoteScreenState extends State<CreateNoteScreen> {
                 ),
               ),
             ),
-
-            // const SizedBox(
-            //   height: 50.0,
-            // ),
-            // Expanded(
-            //   flex: 5,
-            //   child: Container(
-            //     padding: const EdgeInsets.symmetric(
-            //       horizontal: 10.0,
-            //       vertical: 15.0,
-            //     ),
-            //     child: QuillEditor.basic(
-            //       controller: _quillController,
-            //       readOnly: false,
-            //     ),
-            //   ),
-            // ),
-            // Visibility(
-            //   visible: !_isEditNote,
-            //   child: Align(
-            //     alignment: FractionalOffset.bottomCenter,
-            //     child: CircleButton(
-            //       onPressed: () {
-            //         setState(() {
-            //           _isEditNote = true;
-            //         });
-            //       },
-            //       icon: Icons.edit_rounded,
-            //     ),
-            //   ),
-            // ),
-
-            // Expanded(
-            //   child: Stack(
-            //     children: [
-            //       Expanded(
-            //         child: Padding(
-            //           padding: const EdgeInsets.symmetric(
-            //             horizontal: 10.0,
-            //             vertical: 15.0,
-            //           ),
-            //           child: QuillEditor.basic(
-            //             controller: _quillController,
-            //             readOnly: false,
-            //           ),
-            //         ),
-            //       ),
-            //       Visibility(
-            //         visible: !_isEditNote,
-            //         child: Align(
-            //           alignment: FractionalOffset.bottomCenter,
-            //           child: Padding(
-            //             padding: const EdgeInsets.only(bottom: 20.0),
-            //             child: CircleButton(
-            //               onPressed: () {
-            //                 setState(() {
-            //                   _isEditNote = true;
-            //                 });
-            //               },
-            //               icon: Icons.edit_rounded,
-            //             ),
-            //           ),
-            //         ),
-            //       ),
-            //     ],
-            //   ),
-            // ),
-            // Visibility(
-            //   visible: !_isEditNote,
-            //   child: CircleButton(
-            //     onPressed: () {
-            //       setState(() {
-            //         _isEditNote = true;
-            //       });
-            //     },
-            //     icon: Icons.edit_rounded,
-            //   ),
-            // ),
           ],
         ),
       ),
