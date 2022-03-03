43cf9758112b886dd4d4576e1105a76f92182877
Usman <usmanshabir65@hotmail.com>
2022-03-03 20:12:27 +0000

    Custom linear progress bar widget name changed

--- lib/views/loading_screen.dart
+++ lib/views/loading_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:note_taking_app/components/linear_progress_bar.dart';
+import 'package:note_taking_app/components/linear_loading_progress_bar.dart';
 import 'package:note_taking_app/utilities/constants.dart';
 import 'package:note_taking_app/viewModels/loading_screen_view_model.dart';
 
@@ -44,7 +44,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
               width: 50.0,
             ),
             // Loading progress bar
-            LinearProgressBar(
+            LinearLoadingProgressBar(
               key: Key('linearProgressBar'),
             ),
             SizedBox(
