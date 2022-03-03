4a5150fa00ca7e171ffd97e756dbb07f491e6a08
Usman <usmanshabir65@hotmail.com>
2022-03-03 22:08:40 +0000

    Modified LinearProgressBar added

--- lib/views/loading_screen.dart
+++ lib/views/loading_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:note_taking_app/components/linear_loading_progress_bar.dart';
+import 'package:note_taking_app/components/linear_progress_bar.dart';
 import 'package:note_taking_app/utilities/constants.dart';
 import 'package:note_taking_app/viewModels/loading_screen_view_model.dart';
 
@@ -44,7 +44,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
               width: 50.0,
             ),
             // Loading progress bar
-            LinearLoadingProgressBar(
+            LinearProgressBar(
               key: Key('linearProgressBar'),
               backgroundColour: Color(0xFF4CAF50),
               colour: Color(0xFFC8E6C9),
