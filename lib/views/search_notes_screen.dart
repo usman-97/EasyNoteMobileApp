10adbe8f262b3e7f35af206f6710488324133bf5
Usman <usmanshabir65@hotmail.com>
2022-03-04 17:03:40 +0000

    StreamBuilder added to update user notes

--- lib/views/search_notes_screen.dart
+++ lib/views/search_notes_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:note_taking_app/utilities/constants.dart';
+import 'package:note_taking_app/viewModels/note_list_view_model.dart';
+import 'package:note_taking_app/viewModels/search_notes_view_model.dart';
+import 'package:note_taking_app/components/note_card.dart';
 
 class SearchNoteScreen extends StatefulWidget {
   static const String id = 'search_note_screen';
@@ -13,10 +16,14 @@ class SearchNoteScreen extends StatefulWidget {
 class _SearchNoteScreenState extends State<SearchNoteScreen> {
   final TextEditingController _searchTextFieldController =
       TextEditingController();
+  final SearchNotesViewModel _searchNotesViewModel = SearchNotesViewModel();
+  final NoteListViewModel _noteListViewModel = NoteListViewModel();
+  // String _keyword = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: kLightPrimaryColour,
       appBar: AppBar(
         leading: TextButton(
           onPressed: () {
@@ -30,21 +37,55 @@ class _SearchNoteScreenState extends State<SearchNoteScreen> {
         ),
         title: TextField(
           controller: _searchTextFieldController,
-          style: kAppBarTextFieldStyle.copyWith(color: kTextIconColour),
-          decoration: kNoteTitleInputDecoration.copyWith(
-            hintText: 'Search ...',
-            hintStyle: const TextStyle(color: kTextIconColour),
+          onChanged: (value) {
+            if (value.isNotEmpty) {
+              _searchNotesViewModel.keyword = value;
+              // print(_searchNotesViewModel.keyword);
+              _searchNotesViewModel.getAllSearchUserNotes();
+            }
+          },
+          style: kAppBarTextFieldStyle.copyWith(color: kPrimaryTextColour),
+          decoration: kRoundTextFieldInputDecoration.copyWith(
+            fillColor: kTextIconColour,
+            hintStyle: const TextStyle(color: kTextFieldHintColour),
+            hintText: 'Search...',
           ),
         ),
         actions: <Widget>[
           TextButton(
-              onPressed: () {},
-              child: const Icon(
-                Icons.search_rounded,
-                color: kTextIconColour,
-                size: 40.0,
-              ))
+            onPressed: () {},
+            child: const Icon(
+              Icons.search_rounded,
+              color: kTextIconColour,
+              size: 40.0,
+            ),
+          ),
         ],
+      ),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(
+              child: StreamBuilder(
+                stream: _searchNotesViewModel.streamController.stream,
+                builder:
+                    (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+                  if (!snapshot.hasData) {
+                    return Container();
+                  }
+
+                  List<NoteCard> searchedNoteCards =
+                      _noteListViewModel.buildUserNoteCards(snapshot, context);
+
+                  return ListView(
+                    children: searchedNoteCards,
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
