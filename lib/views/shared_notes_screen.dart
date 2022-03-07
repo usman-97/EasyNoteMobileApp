50f3e7db2fcdd8ff32b6805c4287925572debadb
Usman <usmanshabir65@hotmail.com>
2022-03-07 23:15:17 +0000

    Shared notes screen created

---
+++ lib/views/shared_notes_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SharedNoteScreen extends StatefulWidget {
+  const SharedNoteScreen({Key? key}) : super(key: key);
+
+  static const String id = 'shared_notes_screen';
+
+  @override
+  State<SharedNoteScreen> createState() => _SharedNoteScreenState();
+}
+
+class _SharedNoteScreenState extends State<SharedNoteScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
