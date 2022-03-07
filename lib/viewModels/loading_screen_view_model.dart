5d60e46c1a10764c234076d5df1757273ccc05ec
Usman <usmanshabir65@hotmail.com>
2022-03-07 19:53:56 +0000

    Unverifed user is directed to verification screen

--- lib/viewModels/loading_screen_view_model.dart
+++ lib/viewModels/loading_screen_view_model.dart
@@ -4,17 +4,25 @@ import 'package:flutter/material.dart';
 import 'package:note_taking_app/models/user_authentication.dart';
 import 'package:note_taking_app/views/home_screen.dart';
 import 'package:note_taking_app/views/login_screen.dart';
+import 'package:note_taking_app/views/verification_screen.dart';
 
 class LoadingScreenViewModel {
   final UserAuthentication _userAuthentication = UserAuthentication();
 
   void appLoading(BuildContext context) {
-    Future.delayed(const Duration(seconds: 2, milliseconds: 500), () {
+    Future.delayed(const Duration(seconds: 2, milliseconds: 500), () async {
       if (_userAuthentication.getCurrentUser() == null) {
         Navigator.pushNamed(context, LoginScreen.id);
         // print('User has not been Signed in');
       } else {
-        Navigator.pushNamed(context, HomeScreen.id);
+        bool isUserEmailVerified =
+            await _userAuthentication.isUserEmailVerified();
+        if (isUserEmailVerified) {
+          Navigator.pushNamed(context, HomeScreen.id);
+        } else {
+          await _userAuthentication.sendEmailVerification();
+          Navigator.pushNamed(context, VerificationScreen.id);
+        }
         // print('User has been Signed in');
       }
     });
