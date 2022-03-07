a309cef6f1d46478b0133ada21ae8e988c26ee97
Usman <usmanshabir65@hotmail.com>
2022-03-07 18:54:53 +0000

    Check if user exist

--- lib/models/user_management.dart
+++ lib/models/user_management.dart
@@ -1,8 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:note_taking_app/services/firestore_cloud.dart';
+import 'package:note_taking_app/models/user_authentication.dart';
+
+import '../services/firestore_cloud.dart';
 
 class UserManagement {
   final FirebaseFirestore _firestore = FirestoreCloud.firebaseCloudInstance();
+  final UserAuthentication _userAuthentication = UserAuthentication();
+  late String _currentUserEmail;
+
+  UserManagement() {
+    _currentUserEmail = _userAuthentication.getCurrentUserEmail();
+  }
 
   // Add user data to database
   Future<void> addUserData(
@@ -12,5 +20,27 @@ class UserManagement {
       'firstname': firstname,
       'lastname': lastname,
     });
+  }
+
+  Future<void> doesUserExist(String email) async {
+    print(_currentUserEmail);
+    _firestore
+        .collection('users')
+        .where('email', isEqualTo: email)
+        .snapshots()
+        .listen((event) {
+      if (event.docs.isNotEmpty &&
+          event.docs.first.toString() != _currentUserEmail) {
+        print('Not Empty');
+      } else {
+        print('Empty');
+      }
+    });
+
+    // if (await otherUser.length > 0) {
+    //   print('Not Empty');
+    // } else {
+    //   print('Empty');
+    // }
   }
 }
