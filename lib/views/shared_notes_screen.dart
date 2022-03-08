ae0dd25f8cb20811d51498463b535d27d6e8756c
Usman <usmanshabir65@hotmail.com>
2022-03-08 01:00:06 +0000

    shared notes screen added

--- lib/views/shared_notes_screen.dart
+++ lib/views/shared_notes_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:note_taking_app/components/app_menu.dart';
+import 'package:note_taking_app/components/custom_app_bar.dart';
+import 'package:note_taking_app/utilities/constants.dart';
+
+import 'home_screen.dart';
 
 class SharedNoteScreen extends StatefulWidget {
   const SharedNoteScreen({Key? key}) : super(key: key);
@@ -12,6 +17,57 @@ class SharedNoteScreen extends StatefulWidget {
 class _SharedNoteScreenState extends State<SharedNoteScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        actions: const <Widget>[
+          CustomAppBar(),
+        ],
+      ),
+      drawer: AppMenu(),
+      backgroundColor: kLightPrimaryColour,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              color: kPrimaryColour,
+              height: 150.0,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, HomeScreen.id);
+                    },
+                    child: const Padding(
+                      padding: EdgeInsets.only(bottom: 10.0),
+                      child: Icon(
+                        Icons.arrow_back_rounded,
+                        color: kTextIconColour,
+                        size: 40.0,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 20.0,
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(bottom: 15.0),
+                    child: Text(
+                      'Shared Notes',
+                      style: TextStyle(
+                        color: kTextIconColour,
+                        fontSize: 40.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
