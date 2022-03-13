652d9ec8d01be4639ad32332009f4df50ea192ab
Usman <usmanshabir65@hotmail.com>
2022-03-13 02:55:17 +0000

    slidable added

--- lib/components/note_card.dart
+++ lib/components/note_card.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'note_pop_up_menu.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class NoteCard extends StatelessWidget {
   const NoteCard({
@@ -10,12 +9,10 @@ class NoteCard extends StatelessWidget {
     required this.last_modified,
     required this.status,
     required this.onTap,
-    required this.notePopUpMenu,
   }) : super(key: key);
 
   final String title, date_created, last_modified, status;
   final Function() onTap;
-  final NotePopUpMenu notePopUpMenu;
 
   @override
   Widget build(BuildContext context) {
@@ -23,47 +20,102 @@ class NoteCard extends StatelessWidget {
       onTap: onTap,
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10.0),
-        child: Card(
-          color: Colors.amberAccent,
-          child: Padding(
-            padding: const EdgeInsets.all(5.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      title,
-                      style: const TextStyle(fontSize: 40.0),
-                    ),
-                    notePopUpMenu,
-                    // TextButton(
-                    //   onPressed: () {},
-                    //   child: const Icon(Icons.more_horiz_rounded),
-                    // ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 10.0,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: <Widget>[
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text('Created: $date_created'),
-                        Text('Last Modified: $last_modified'),
-                      ],
-                    ),
-                    Padding(
+        child: Slidable(
+          key: const ValueKey(0),
+          // startActionPane: ActionPane(
+          //   motion: const DrawerMotion(),
+          //   dismissible: DismissiblePane(
+          //     onDismissed: () {},
+          //   ),
+          //   children: <Widget>[
+          //     SlidableAction(
+          //       onPressed: (value) {},
+          //       backgroundColor: const Color(0xFFFE4A49),
+          //       foregroundColor: Colors.white,
+          //       icon: Icons.delete_rounded,
+          //       label: 'Delete',
+          //     ),
+          //     SlidableAction(
+          //       onPressed: (value) {},
+          //       backgroundColor: const Color(0xFF21B7CA),
+          //       foregroundColor: Colors.white,
+          //       icon: Icons.share_rounded,
+          //       label: 'Share',
+          //     ),
+          //   ],
+          // ),
+          endActionPane: ActionPane(
+            motion: const DrawerMotion(),
+            children: <Widget>[
+              // SlidableAction(
+              //   onPressed: (value) {},
+              //   backgroundColor: const Color(0xFF7BC043),
+              //   foregroundColor: Colors.white,
+              //   icon: Icons.archive_rounded,
+              //   label: 'Archive',
+              // ),
+              // SlidableAction(
+              //   onPressed: (value) {},
+              //   backgroundColor: const Color(0xFF0392CF),
+              //   foregroundColor: Colors.white,
+              //   icon: Icons.save_rounded,
+              //   label: 'Save',
+              // ),
+
+              SlidableAction(
+                onPressed: (value) {},
+                backgroundColor: const Color(0xFF21B7CA),
+                foregroundColor: Colors.white,
+                icon: Icons.share_rounded,
+                label: 'Share',
+              ),
+              SlidableAction(
+                onPressed: (value) {},
+                backgroundColor: const Color(0xFFFE4A49),
+                foregroundColor: Colors.white,
+                icon: Icons.delete_rounded,
+                label: 'Delete',
+              ),
+            ],
+          ),
+          child: Card(
+            color: Colors.amberAccent,
+            child: Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        title,
+                        style: const TextStyle(fontSize: 40.0),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>[
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text('Created: $date_created'),
+                          Text('Last Modified: $last_modified'),
+                        ],
+                      ),
+                      Padding(
                         padding: const EdgeInsets.all(5.0),
-                        child: Text(status)),
-                  ],
-                ),
-              ],
+                        child: Text(status),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
