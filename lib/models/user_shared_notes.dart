ce0906093c986488a031683e27f826da4770a5f7
Usman <usmanshabir65@hotmail.com>
2022-03-14 11:32:02 +0000

    user sharing method improved

--- lib/models/user_shared_notes.dart
+++ lib/models/user_shared_notes.dart
@@ -13,35 +13,81 @@ class UserSharedNotes {
     _userEmail = _userAuthentication.getCurrentUserEmail();
   }
 
-  void shareUserNote(String documentID, String otherUserEmail) async {
-    // print('$documentID $otherUserEmail');
-
-    bool doesUserExist = await _userManagement.doesUserExist(otherUserEmail);
-    bool hasNoteAlreadyShared = await _hasUserAlreadySharedTheNote(documentID);
-
-    if (doesUserExist && !hasNoteAlreadyShared) {
-      _firestore
+  Future<void> addSharedNoteData(String documentID) async {
+    try {
+      // String noteID = '';
+      // late DocumentReference noteRef;
+      await _firestore
           .collection('notes')
           .doc(_userEmail)
-          .collection('shared')
-          .doc()
           .collection('notes')
-          .add({
-        'document': documentID,
-        'owner': _userEmail,
+          .where('id', isEqualTo: documentID)
+          .get()
+          .then((value) {
+        if (value.docs.isNotEmpty) {
+          String noteID = value.docs.first.id;
+          DocumentReference noteRef = _firestore
+              .collection('notes')
+              .doc(_userEmail)
+              .collection('notes')
+              .doc(noteID);
+
+          _firestore.collection('shared_notes').add({
+            'author': _userEmail,
+            'note_id': documentID,
+            'note': noteRef,
+          });
+        }
+        // noteID = value.docs.first.id;
       });
 
-      _firestore
-          .collection('notes')
-          .doc(otherUserEmail)
-          .collection('shared')
-          .doc()
-          .collection('notes')
-          .add({
-        'document': documentID,
-        'owner': _userEmail,
+      // DocumentReference noteRef = _firestore
+      //     .collection('notes')
+      //     .doc(_userEmail)
+      //     .collection('notes')
+      //     .doc(noteID);
+
+      // await _firestore.collection('shared_notes').add({
+      //   'author': _userEmail,
+      //   'note_id': documentID,
+      //   'note': noteRef,
+      // });
+    } on FirebaseException catch (e) {}
+  }
+
+  Future<void> addUsers(
+      String email, String userAccess, String documentID) async {
+    try {
+      // String noteID = '';
+      await _firestore
+          .collection('shared_notes')
+          .where('note_id', isEqualTo: documentID)
+          .where('author', isEqualTo: _userEmail)
+          .get()
+          .then((value) {
+        if (value.docs.isNotEmpty) {
+          String noteID = value.docs.first.id;
+          _firestore
+              .collection('shared_notes')
+              .doc(noteID)
+              .collection('user_with_access')
+              .add({
+            'user': email,
+            'access': userAccess,
+          });
+        }
+        // noteID = value.docs.first.id;
       });
-    }
+
+      // await _firestore
+      //     .collection('shared_notes')
+      //     .doc(noteID)
+      //     .collection('user_with_access')
+      //     .add({
+      //   'user': email,
+      //   'access': userAccess,
+      // });
+    } on FirebaseException catch (e) {}
   }
 
   Future<bool> _hasUserAlreadySharedTheNote(String documentID) async {
@@ -61,5 +107,27 @@ class UserSharedNotes {
     }
 
     return hasNoteAlreadyShared;
+  }
+
+  Future<bool> isNoteAlreadyBeenSharedWithUser(
+      String documentID, String email) async {
+    bool isNoteSharedWithUser = false;
+    try {
+      String noteID = '';
+      await _firestore
+          .collection('shared_notes')
+          .where('author', isEqualTo: _userEmail)
+          .where('note_id', isEqualTo: documentID)
+          .get()
+          .then((value) {
+        if (value.docs.isNotEmpty) {
+          noteID = value.docs.first.id;
+        }
+      });
+
+      await _firestore.collection('shared_notes').doc(noteID);
+    } on FirebaseException catch (e) {}
+
+    return isNoteSharedWithUser;
   }
 }
