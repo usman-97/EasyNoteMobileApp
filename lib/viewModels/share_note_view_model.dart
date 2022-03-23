b194a8be59e8d2a6902fd70ce9245d2bed7cb334
Usman <usmanshabir65@hotmail.com>
2022-03-23 01:47:00 +0000

    user existance validation added

--- lib/viewModels/share_note_view_model.dart
+++ lib/viewModels/share_note_view_model.dart
@@ -1,16 +1,31 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:note_taking_app/models/user_management.dart';
 import 'package:note_taking_app/models/user_shared_notes.dart';
 
 class ShareNoteViewModel {
   final UserSharedNotes _userSharedNotes = UserSharedNotes();
+  final UserManagement _userManagement = UserManagement();
+  String _error = '';
 
-  void shareUserNote(String documentID, String email, String userAccess) async {
+  get error => _error;
+
+  Future<void> shareUserNote(
+      String documentID, String email, String userAccess) async {
     bool isNotAlreadySharedWithUser = await _userSharedNotes
         .isNoteAlreadyBeenSharedWithUser(documentID, email);
-    // print(isNotAlreadySharedWithUser);
-    if (email.isNotEmpty && !isNotAlreadySharedWithUser) {
-      await _userSharedNotes.addSharedNoteData(documentID);
-      await _userSharedNotes.addUsers(email, userAccess, documentID);
-      await _userSharedNotes.updateNoteStatus(documentID);
+    bool isUserExist = await _userManagement.isUserExist(email);
+    // print(isUserExist);
+    if (email.isNotEmpty && isUserExist) {
+      if (!isNotAlreadySharedWithUser) {
+        _error = '';
+        await _userSharedNotes.addSharedNoteData(documentID);
+        await _userSharedNotes.addUsers(email, userAccess, documentID);
+        await _userSharedNotes.updateNoteStatus(documentID);
+      } else {
+        _error = 'This note has already been shared with $email';
+      }
+    } else {
+      _error = 'Invalid email';
     }
   }
 }
