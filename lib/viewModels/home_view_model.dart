b31b96ae35a9f98d2a3902fbf11ad2581867b0a7
Usman <usmanshabir65@hotmail.com>
2022-03-23 01:18:21 +0000

    Get user firstname and today date

--- lib/viewModels/home_view_model.dart
+++ lib/viewModels/home_view_model.dart
@@ -1,1 +1,24 @@
-class HomeViewModel {}
+import 'package:note_taking_app/models/user_management.dart';
+import 'package:intl/intl.dart';
+
+class HomeViewModel {
+  final UserManagement _userManagement = UserManagement();
+  String _userFirstname = '';
+
+  get userFirstname => _userFirstname;
+
+  Stream<String> setUserFirstName() {
+    return _userManagement.fetchCurrentUserFirstname();
+  }
+
+  String getTodayData() {
+    DateTime date = DateTime.now().toLocal();
+    String weekDay = DateFormat.EEEE().format(date);
+    String day = date.day.toString();
+    String month = DateFormat.MMMM().format(date);
+    String year = date.year.toString();
+    String todayDate = '$weekDay, $day $month $year';
+
+    return todayDate;
+  }
+}
