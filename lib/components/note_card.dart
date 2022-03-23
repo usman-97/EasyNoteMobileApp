dade2c6c3e0aeb06deaa3acc9fccd748b668a1f1
Usman <usmanshabir65@hotmail.com>
2022-03-23 01:17:26 +0000

    Background colour changed and border added

--- lib/components/note_card.dart
+++ lib/components/note_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:note_taking_app/utilities/constants.dart';
 
 class NoteCard extends StatelessWidget {
   const NoteCard({
@@ -22,71 +23,71 @@ class NoteCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10.0),
-        child: status != 'Read-only' && status != 'Read/Write'
-            ? Slidable(
-                key: const ValueKey(0),
-                // startActionPane: ActionPane(
-                //   motion: const DrawerMotion(),
-                //   dismissible: DismissiblePane(
-                //     onDismissed: () {},
-                //   ),
-                //   children: <Widget>[
-                //     SlidableAction(
-                //       onPressed: (value) {},
-                //       backgroundColor: const Color(0xFFFE4A49),
-                //       foregroundColor: Colors.white,
-                //       icon: Icons.delete_rounded,
-                //       label: 'Delete',
-                //     ),
-                //     SlidableAction(
-                //       onPressed: (value) {},
-                //       backgroundColor: const Color(0xFF21B7CA),
-                //       foregroundColor: Colors.white,
-                //       icon: Icons.share_rounded,
-                //       label: 'Share',
-                //     ),
-                //   ],
-                // ),
-                endActionPane: ActionPane(
-                  motion: const DrawerMotion(),
-                  children: <Widget>[
-                    // SlidableAction(
-                    //   onPressed: (value) {},
-                    //   backgroundColor: const Color(0xFF7BC043),
-                    //   foregroundColor: Colors.white,
-                    //   icon: Icons.archive_rounded,
-                    //   label: 'Archive',
-                    // ),
-                    // SlidableAction(
-                    //   onPressed: (value) {},
-                    //   backgroundColor: const Color(0xFF0392CF),
-                    //   foregroundColor: Colors.white,
-                    //   icon: Icons.save_rounded,
-                    //   label: 'Save',
-                    // ),
+      child: status != 'Read-only' && status != 'Read/Write'
+          ? Slidable(
+              key: const ValueKey(0),
+              // startActionPane: ActionPane(
+              //   motion: const DrawerMotion(),
+              //   dismissible: DismissiblePane(
+              //     onDismissed: () {},
+              //   ),
+              //   children: <Widget>[
+              //     SlidableAction(
+              //       onPressed: (value) {},
+              //       backgroundColor: const Color(0xFFFE4A49),
+              //       foregroundColor: Colors.white,
+              //       icon: Icons.delete_rounded,
+              //       label: 'Delete',
+              //     ),
+              //     SlidableAction(
+              //       onPressed: (value) {},
+              //       backgroundColor: const Color(0xFF21B7CA),
+              //       foregroundColor: Colors.white,
+              //       icon: Icons.share_rounded,
+              //       label: 'Share',
+              //     ),
+              //   ],
+              // ),
+              endActionPane: ActionPane(
+                motion: const DrawerMotion(),
+                children: <Widget>[
+                  // SlidableAction(
+                  //   onPressed: (value) {},
+                  //   backgroundColor: const Color(0xFF7BC043),
+                  //   foregroundColor: Colors.white,
+                  //   icon: Icons.archive_rounded,
+                  //   label: 'Archive',
+                  // ),
+                  // SlidableAction(
+                  //   onPressed: (value) {},
+                  //   backgroundColor: const Color(0xFF0392CF),
+                  //   foregroundColor: Colors.white,
+                  //   icon: Icons.save_rounded,
+                  //   label: 'Save',
+                  // ),
 
-                    SlidableAction(
-                      onPressed: onShare ?? (context) {},
-                      backgroundColor: const Color(0xFF21B7CA),
-                      foregroundColor: Colors.white,
-                      icon: Icons.share_rounded,
-                      label: 'Share',
-                    ),
-                    SlidableAction(
-                      onPressed: (value) {},
-                      backgroundColor: const Color(0xFFFE4A49),
-                      foregroundColor: Colors.white,
-                      icon: Icons.delete_rounded,
-                      label: 'Delete',
-                    ),
-                  ],
-                ),
-                child: Card(
-                  color: Colors.amberAccent,
+                  SlidableAction(
+                    onPressed: onShare ?? (context) {},
+                    backgroundColor: const Color(0xFF21B7CA),
+                    foregroundColor: Colors.white,
+                    icon: Icons.share_rounded,
+                    label: 'Share',
+                  ),
+                  SlidableAction(
+                    onPressed: (value) {},
+                    backgroundColor: const Color(0xFFFE4A49),
+                    foregroundColor: Colors.white,
+                    icon: Icons.delete_rounded,
+                    label: 'Delete',
+                  ),
+                ],
+              ),
+              child: Card(
+                color: kLightPrimaryColour,
+                child: Container(
+                  decoration: kNoteCardBorder,
                   child: Padding(
-                    padding: const EdgeInsets.all(5.0),
+                    padding: const EdgeInsets.all(8.0),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.stretch,
                       children: <Widget>[
@@ -123,11 +124,14 @@ class NoteCard extends StatelessWidget {
                     ),
                   ),
                 ),
-              )
-            : Card(
-                color: Colors.amberAccent,
+              ),
+            )
+          : Card(
+              color: kLightPrimaryColour,
+              child: Container(
+                decoration: kNoteCardBorder,
                 child: Padding(
-                  padding: const EdgeInsets.all(5.0),
+                  padding: const EdgeInsets.all(8.0),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: <Widget>[
@@ -178,7 +182,7 @@ class NoteCard extends StatelessWidget {
                   ),
                 ),
               ),
-      ),
+            ),
     );
   }
 }
