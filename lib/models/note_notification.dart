6452f2c29fbb029028aeab8cd0cb6d02fd811599
Usman <usmanshabir65@hotmail.com>
2022-03-28 18:09:35 +0100

    Delete request method added

--- lib/models/note_notification.dart
+++ lib/models/note_notification.dart
@@ -1,21 +1,31 @@
 import 'dart:async';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:note_taking_app/models/data/sharing_request_data.dart';
 import 'package:note_taking_app/models/user_authentication.dart';
+import 'package:note_taking_app/models/user_management.dart';
+import 'package:note_taking_app/models/user_note.dart';
 import 'package:note_taking_app/services/firestore_cloud.dart';
 
-class Notification {
+class NoteNotification {
   final FirebaseFirestore _firestore = FirestoreCloud.firebaseCloudInstance();
   final UserAuthentication _userAuthentication = UserAuthentication();
+  final UserManagement _userManagement = UserManagement();
+  final UserNote _userNote = UserNote();
   late String _userEmail;
 
-  Notification() {
+  NoteNotification() {
     _userEmail = _userAuthentication.getCurrentUserEmail();
   }
 
   Future<void> sendSharingRequest(
       String recipientEmail, String documentID, String access) async {
+    String userName = await _userManagement.fetchUserFullName();
+    String noteTitle = await _userNote.fetchNoteTitle(documentID);
+
+    print('name: $userName title: $noteTitle');
+
     try {
       _firestore
           .collection('notifications')
@@ -23,8 +33,10 @@ class Notification {
           .collection('sharing_requests')
           .add({
         'sender': _userEmail,
+        'sender_name': userName,
         'recipient': recipientEmail,
         'note': documentID,
+        'note_title': noteTitle,
         'access': access,
         'status': 'unread',
       });
@@ -69,5 +81,40 @@ class Notification {
     } on FirebaseException catch (e) {}
 
     return controller.stream;
+  }
+
+  Future<void> deleteSharingRequest(
+      String sender, String note, String access) async {
+    CollectionReference sharingRequestColRef = _firestore
+        .collection('notifications')
+        .doc(_userEmail)
+        .collection('sharing_requests');
+    try {
+      await sharingRequestColRef
+          .where('sender', isEqualTo: sender)
+          .where('recipient', isEqualTo: _userEmail)
+          .where('note', isEqualTo: note)
+          .get()
+          .then((value) async {
+        if (value.docs.isNotEmpty) {
+          await sharingRequestColRef.doc(value.docs.first.id).delete();
+        }
+      });
+    } on FirebaseException catch (e) {}
+  }
+
+  Future<void> deleteSharingNotePendingRequest(
+      String sender, String recipient, String note, String access) async {
+    CollectionReference sharingRequestColRef = _firestore
+        .collection('notifications')
+        .doc(sender)
+        .collection('sharing_requests');
+    try {
+      await sharingRequestColRef.get().then((value) async {
+        if (value.docs.isNotEmpty) {
+          await sharingRequestColRef.doc(value.docs.first.id).delete();
+        }
+      });
+    } on FirebaseException catch (e) {}
   }
 }
