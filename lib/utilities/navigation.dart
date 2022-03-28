c445a016859a8ff38a553f8e472ec1716fe671e8
Usman <usmanshabir65@hotmail.com>
2022-03-28 18:16:16 +0100

    navigateToNotificationScreen method added

--- lib/utilities/navigation.dart
+++ lib/utilities/navigation.dart
@@ -3,6 +3,7 @@ import 'package:note_taking_app/views/create_note_screen.dart';
 import 'package:note_taking_app/views/home_screen.dart';
 import 'package:note_taking_app/views/login_screen.dart';
 import 'package:note_taking_app/views/note_list_screen.dart';
+import 'package:note_taking_app/views/notifications_screen.dart';
 import 'package:note_taking_app/views/register_screen.dart';
 import 'package:note_taking_app/views/search_notes_screen.dart';
 import 'package:note_taking_app/views/share_note_screen.dart';
@@ -44,5 +45,9 @@ class Navigation {
 
   static void navigateToShareNoteScreen(BuildContext context) {
     Navigator.pushNamed(context, ShareNoteScreen.id);
+  }
+
+  static void navigateToNotificationsScreen(BuildContext context) {
+    Navigator.popAndPushNamed(context, NotificationsScreen.id);
   }
 }
