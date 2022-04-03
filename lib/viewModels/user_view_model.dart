4228e3b50807d8f14fd67f80b0fed4141f4d12f8
Usman <usmanshabir65@hotmail.com>
2022-04-03 02:35:20 +0100

    get user full name

--- lib/viewModels/user_view_model.dart
+++ lib/viewModels/user_view_model.dart
@@ -1,7 +1,13 @@
 import 'package:note_taking_app/models/user_authentication.dart';
+import 'package:note_taking_app/models/user_management.dart';
 
 class UserViewModel {
   final UserAuthentication _userAuthentication = UserAuthentication();
+  final UserManagement _userManagement = UserManagement();
+
+  Future<String> getUserFullName() {
+    return _userManagement.fetchUserFullName();
+  }
 
   void signOutUser() {
     _userAuthentication.signOutUser();
