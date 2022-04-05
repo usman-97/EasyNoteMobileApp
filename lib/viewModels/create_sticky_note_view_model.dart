644773eb9a0c83bf14797176a822e6d0e8afcfcd
Usman <usmanshabir65@hotmail.com>
2022-04-05 03:24:41 +0100

    Documentation added

--- lib/viewModels/create_sticky_note_view_model.dart
+++ lib/viewModels/create_sticky_note_view_model.dart
@@ -5,6 +5,10 @@ class CreateStickyNoteViewModel {
   final UserStickyNotes _userStickyNotes = UserStickyNotes();
   final CustomDate _date = CustomDate();
 
+  /// Add new sticky note with new [noteID] and [newNoteTitle]
+  /// if sticky note already exist then update the last modified date
+  /// check if [noteTitle] and [newNoteTitle] are same
+  /// if not then update sticky note title
   Future<void> addUserStickyNote(
       String noteID, String noteTitle, String newNoteTitle) async {
     String date = _date.getShortFormatDate();
