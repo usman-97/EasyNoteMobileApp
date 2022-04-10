006ce63821885fa07ea41ca816bd69b8733ea529
Usman <usmanshabir65@hotmail.com>
2022-04-10 01:48:05 +0100

    LastModifiedTime field added

--- lib/models/data/user_note_data.dart
+++ lib/models/data/user_note_data.dart
@@ -2,40 +2,40 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class UserNoteData {
   final String documentID,
-      note_title,
-      date_created,
-      last_modified,
+      noteTitle,
+      dateCreated,
+      lastModified,
       lastModifiedTime,
       status;
   UserNoteData(
       {required this.documentID,
-      required this.note_title,
-      required this.date_created,
-      required this.last_modified,
+      required this.noteTitle,
+      required this.dateCreated,
+      required this.lastModified,
       required this.lastModifiedTime,
       required this.status});
 
   UserNoteData.fromDocumentSnapshot(DocumentSnapshot snapshot)
       : documentID = snapshot.get('id'),
-        note_title = snapshot.get('title'),
-        date_created = snapshot.get('date_created'),
-        last_modified = snapshot.get('last_modified'),
+        noteTitle = snapshot.get('title'),
+        dateCreated = snapshot.get('date_created'),
+        lastModified = snapshot.get('last_modified'),
         lastModifiedTime = snapshot.get('last_modified_time'),
         status = snapshot.get('status');
 
   UserNoteData.fromJson(Map<String, dynamic> jsonData)
       : documentID = jsonData['id'],
-        note_title = jsonData['title'],
-        date_created = jsonData['date_created'],
-        last_modified = jsonData['last_modified'],
+        noteTitle = jsonData['title'],
+        dateCreated = jsonData['date_created'],
+        lastModified = jsonData['last_modified'],
         lastModifiedTime = jsonData['last_modified_time'],
         status = jsonData['status'];
 
   Map<String, dynamic> toJson() => {
         'id': documentID,
-        'title': note_title,
-        'date_created': date_created,
-        'last_modified': last_modified,
+        'title': noteTitle,
+        'date_created': dateCreated,
+        'last_modified': lastModified,
         'status': status,
       };
 
