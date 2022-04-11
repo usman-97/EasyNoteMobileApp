792276a2a65980760e2eed85b184653fa2cc5147
Usman <usmanshabir65@hotmail.com>
2022-04-11 19:33:45 +0100

    Get selected filename without extension

--- lib/utilities/file_handler.dart
+++ lib/utilities/file_handler.dart
@@ -5,6 +5,7 @@ import 'dart:typed_data';
 import 'package:file_saver/file_saver.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
+import 'package:path/path.dart';
 
 class FileHandler {
   void shareNoteAsJson(String noteID) async {
@@ -17,5 +18,9 @@ class FileHandler {
     List<int> encodedFile = utf8.encode(file.toString());
     Uint8List data = Uint8List.fromList(encodedFile);
     await FileSaver.instance.saveAs(filename, data, 'json', MimeType.JSON);
+  }
+
+  String getFilename(String path) {
+    return basenameWithoutExtension(path);
   }
 }
