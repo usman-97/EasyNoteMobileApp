8663ffd9c14bf78a6a9daa50be38447b2f6602fe
Usman <usmanshabir65@hotmail.com>
2022-04-11 02:11:08 +0100

    File handler class added

---
+++ lib/utilities/file_handler.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:file_saver/file_saver.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:share_plus/share_plus.dart';
+
+class FileHandler {
+  void shareNoteAsJson(String noteID) async {
+    Directory dir = await getTemporaryDirectory();
+
+    Share.shareFiles(['${dir.path}/files/$noteID']);
+  }
+
+  Future<void> saveFileAsJson(String file, String filename) async {
+    List<int> encodedFile = utf8.encode(file.toString());
+    Uint8List data = Uint8List.fromList(encodedFile);
+    await FileSaver.instance.saveAs(filename, data, 'json', MimeType.JSON);
+  }
+}
