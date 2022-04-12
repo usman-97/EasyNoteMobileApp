3970559dc8e9be8f624a49c19fac6d577eb5bfcd
Usman <usmanshabir65@hotmail.com>
2022-04-12 01:32:11 +0100

    Add note button test added

--- test/widget_test/home_screen_test.dart
+++ test/widget_test/home_screen_test.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:note_taking_app/components/app_menu.dart';
 import 'package:note_taking_app/components/circle_button.dart';
 import 'package:note_taking_app/components/custom_app_bar.dart';
 import 'package:note_taking_app/services/initialise_firebase_mock.dart';
@@ -19,13 +18,26 @@ void main() {
       await tester.pumpWidget(const MaterialApp(
         home: HomeScreen(),
       ));
-      await tester.pump(const Duration(seconds: 30));
 
       final appBar = find.byType(CustomAppBar);
       final addNoteButton = find.byType(CircleButton);
+      final notificationButton = find.byKey(const Key('notification_button'));
 
       expect(appBar, findsOneWidget);
       expect(addNoteButton, findsOneWidget);
+      expect(notificationButton, findsOneWidget);
+    });
+
+    testWidgets('Open bottomNavBar when + button is tapped',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(const MaterialApp(home: HomeScreen()));
+
+      final bottomNavBar = find.byKey(const Key('choose_note_type'));
+
+      await tester.tap(find.byType(CircleButton));
+      await tester.pump(const Duration(seconds: 5));
+
+      expect(bottomNavBar, findsOneWidget);
     });
   });
 }
