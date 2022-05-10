c44e30b3204e732682a8c4af684e277b6cbb1e45
Usman <usmanshabir65@hotmail.com>
2022-05-10 22:07:59 +0100

    Commented code removed

--- lib/models/data/user_note_data.dart
+++ lib/models/data/user_note_data.dart
@@ -38,44 +38,4 @@ class UserNoteData {
         'last_modified': lastModified,
         'status': status,
       };
-
-  // Map<String, dynamic> toMap() {
-  //   return {
-  //     'id': documentID,
-  //     'title': note_title,
-  //     'date_created': date_created,
-  //     'last_modified': last_modified,
-  //     'status': status,
-  //   };
-  // }
-  //
-  // static fromMap(Map<String, dynamic> map) {
-  //   if (map == null) return null;
-  //
-  //   return UserNoteData(
-  //     documentID: map['id'],
-  //     note_title: map['note_title'],
-  //     date_created: map['date_created'],
-  //     last_modified: map['last_modified'],
-  //     status: map['status'],
-  //   );
-  // }
-
-  // UserNoteData(QueryDocumentSnapshot<Map<String, dynamic>> data) {
-  //   _documentID = data.get('id');
-  //   _note_title = data.get('title');
-  //   _date_created = data.get('date_created');
-  //   _last_modified = data.get('last_modified');
-  //   _status = data.get('status');
-  // }
-  //
-  // get status => _status;
-  //
-  // get last_modified => _last_modified;
-  //
-  // get date_created => _date_created;
-  //
-  // get note_title => _note_title;
-  //
-  // String get documentID => _documentID;
 }
