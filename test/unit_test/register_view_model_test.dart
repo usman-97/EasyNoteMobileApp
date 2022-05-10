6bb98a5b1101293bb28bfb3a5f0023ae5bcf2ce1
Usman <usmanshabir65@hotmail.com>
2022-05-10 21:27:23 +0100

    Unit tests

---
+++ test/unit_test/register_view_model_test.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:note_taking_app/services/initialise_firebase_mock.dart';
+import 'package:note_taking_app/viewModels/home_view_model.dart';
+import 'package:intl/intl.dart';
+
+Future<void> main() async {
+  setupFirebaseAuthMocks();
+  await Firebase.initializeApp();
+
+  // setUpAll(() async {
+  //   await Firebase.initializeApp();
+  // });
+
+  test('Get today date', () {
+    HomeViewModel homeViewModel = HomeViewModel();
+
+    DateTime date = DateTime.now().toLocal();
+    String weekDay = DateFormat.EEEE().format(date);
+    String day = date.day.toString();
+    String month = DateFormat.MMMM().format(date);
+    String year = date.year.toString();
+    String todayDate = '$weekDay, $day $month $year';
+
+    expect(homeViewModel.getTodayDate(), todayDate);
+  });
+}
